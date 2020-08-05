c4bb90c949b36ba91bc1fab3d60669a5846fc49b
Mircea <badraganmircea@gmail.com>
2020-08-06 01:37:50 +0300

    add executable rights to install scripts

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,5 +1,5 @@
-sh scripts/install-zsh.sh
-sh scripts/install-fzf.sh
-sh scripts/install-hack-fonts.sh
-sh scripts/install-powerlevel9k.sh
+sh scripts/install-zsh.sh &&
+sh scripts/install-fzf.sh &&
+sh scripts/install-hack-fonts.sh &&
+sh scripts/install-powerlevel9k.sh &&
 sh scripts/set-default.sh
